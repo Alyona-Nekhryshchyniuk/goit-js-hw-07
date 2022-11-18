2205f0d9563a57a7ed7b995b47a5f457b3e2b812
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-11-18 12:44:56 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const gallery = document.querySelector(".gallery");
-// const galleryItem = document.createElement(div);
+
 const galleryItem = galleryItems
   .map(({ preview, original, description }) => {
     return `<div class="gallery__item">
@@ -21,41 +21,33 @@ const galleryItem = galleryItems
 gallery.insertAdjacentHTML("afterbegin", galleryItem);
 
 const modal = basicLightbox.create(`
-    <div class="modal">
-    </div>
+<div class="gallery__item">
+<a class="gallery__link" href="">
+  <img
+    class="gallery__image"
+    src=""
+    data-source=""
+    alt=""
+  />
+</a>
+</div>
 `);
-
-// const clear = () => {
-//   console.log("need to to clearance");
-//   modalEl.textContent = "";
-//   modal.close();
-// };
-
-const modalEl = modal.element().querySelector("div");
 
 const imgFullScreen = (e) => {
   e.preventDefault();
-  //   modalEl.textContent = "";
-  let clickedImg = e.target.outerHTML;
-  //   if (modalEl.innerHTML !== "") {
 
-  //   }
-  modalEl.innerHTML = clickedImg;
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
 
-  console.log(modalEl.innerHTML);
+  let img = modal.element().querySelector("img");
+  //   img.dataset.source = e.target.dataset.source;
+  img.src = e.target.src;
+
+  let link = img.closest(".gallery__link");
+  link.href = e.target.dataset.source;
+
   modal.show();
-
-  //   document.addEventListener("click", clear);
 };
 
-if (modal.close()) {
-  console.log("now closed");
-  gallery.addEventListener("click", imgFullScreen);
-}
-
-// const closeFn = () => {
-//   modal.close();
-// };
-// if (modal.show()) {
-//   document.addEventListener("click", closeFn);
-// }
+gallery.addEventListener("click", imgFullScreen);
