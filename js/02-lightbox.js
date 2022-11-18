ce6b06d51de9b1b6c7e3d971745ca15ef13b0cef
Alyona-Nekhryshchyniuk <o.nekhryshchyniuk@gmail.com>
2022-11-18 13:56:27 +0200

    ...

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryItem = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li><a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a></li>`;
+  })
+  .join("");
+
+gallery.insertAdjacentHTML("afterbegin", galleryItem);
+
+let galleryI = new SimpleLightbox(".gallery__item");
+galleryI.on("show.simplelightbox", function () {
+  console.log("ffff");
+});
+
+// gallery.on("error.simplelightbox", function (e) {
+
+// });
